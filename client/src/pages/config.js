a9846ac8fe1043096b9a5a8005df970bd042ef41
rinkugaj <rinkugajbhiye1111@gmail.com>
2026-08-14 22:40:07 +0530

    Fix API_BASE_URL typo in config.js
    Corrected the API base URL to include 'mm' at the end.

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://a6daf08c23605407781cb74ffc1fe2a6-815777749.us-east-1.elb.amazonaws.com"
+const API_BASE_URL = "http://a6daf08c23605407781cb74ffc1fe2a6-815777749.us-east-1.elb.amazonaws.commm"
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
